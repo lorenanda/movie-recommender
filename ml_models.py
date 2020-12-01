b71d1dfe252733e21293a51b5ef03d756672afd4
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-12-01 15:41:54 +0100

    upload nmf using surpise

---
+++ ml_models.py
@@ -0,0 +1,58 @@
+import random
+import pandas as pd
+from collections import defaultdict
+from nmf_train_model import user_rating
+import joblib
+
+# load the model from disk
+svd = loaded_model = joblib.load("svd_model.sav")
+
+
+def predict_new_user_input(algo, user_input, orig_data):
+
+    new_user = pd.DataFrame(user_input, index=[random.randint(
+        10_000, 11_000)], columns=orig_data.columns)
+
+    user_input = pd.DataFrame(new_user.unstack().reset_index())
+    user_input.columns = ["movieId", "userId", "rating"]
+
+    pred = []
+    for i in range(len(user_input)):
+
+        pred1 = algo.predict(
+            uid=user_input["userId"].iloc[i], iid=user_input["movieId"].iloc[i])
+
+        pred.append(pred1)
+    return pred
+
+
+def recommand_n(predictions, n=10):
+
+    # First map the predictions to each user.
+    top_n = defaultdict(list)
+    for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+
+    # Then sort the predictions for each user and retrieve the k highest ones.
+    for uid, user_ratings in top_n.items():
+        user_ratings.sort(key=lambda x: x[1], reverse=True)
+        top_n[uid] = user_ratings[:n]
+
+    recomendacion = []
+    columnas = ["userId", "movieId"]
+    recomendaciones = pd.DataFrame(columns=columnas)
+    for uid, user_ratings in top_n.items():
+        recomendacion = [iid for (iid, _) in user_ratings]
+        for rec in recomendacion:
+            agregar = {'userId': uid, 'movieId': rec}
+            recomendaciones = recomendaciones.append(
+                agregar, ignore_index=True)
+
+    return recomendaciones
+
+
+if __name__ == "__main__":
+    new_user_input = {1: 3, 50: 4}
+    pred = predict_new_user_input(
+        algo=svd, user_input=new_user_input, orig_data=user_rating)
+    recommand_n(pred, 10)
