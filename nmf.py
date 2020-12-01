7decb0cbf780e04bae95398d79c9acf615baa383
lorenanda <datalingo@outlook.com>
2020-12-01 21:06:31 +0100

    added slider for movie ratings

--- nmf.py
+++ nmf.py
@@ -9,9 +9,10 @@ movies = pd.read_csv('./data/movies.csv')
 ratings = pd.read_csv('./data/ratings.csv')
 ratings_pivot = ratings.pivot(index='userId', columns='movieId', values='rating')
 ratings_pivot.replace(np.nan, 0, inplace=True)
-#convert ratings to dense?
+
 
 if __name__ == "__main__":
+
     model = NMF(
         n_components=20,
         init='random',
@@ -21,7 +22,6 @@ if __name__ == "__main__":
     model.fit(ratings_pivot)
     model.reconstruction_err_
     joblib.dump(model,"nmf.sav")
-
 
     P = model.transform(ratings_pivot) 
     Q = model.components_.T
