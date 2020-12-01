2f2ca7b3c46d0fe45b7a24c821a0578762b81a4b
lorenanda <datalingo@outlook.com>
2020-12-01 18:08:41 +0100

    updates after meeting

--- recommender.py
+++ recommender.py
@@ -5,7 +5,10 @@ that makes recommendatins based on movie ratings.
 
 import pandas as pd
 import random
-from sklearn.decomposition import NMF
+from nmf import ratings_pivot
+import joblib
+
+model=joblib.load("nmf.sav")
 
 movies_df = pd.read_csv('./data/movies.csv')
 movies = movies_df['title']
@@ -14,21 +17,5 @@ def get_recommendations():
     random.shuffle(movies)
     return movies[:3]
 
-def nmf_recommender():
+def nmf_recommender(model, orig_data):
     pass
-
-    """
-    ratings = pd.read_csv('./data/ratings.csv')
-    ratings_pivot = ratings.pivot(index='userId', columns='movieId', values='rating')
-    ratings_pivot.replace(np.nan, 0, inplace=True)
-    #convert ratings to dense?
-
-    model = NMF(n_components=2, init='random', random_state=10)
-    model.fit(ratings_pivot)
-
-    P = model.transform(ratings_pivot) 
-    Q = model.components_.T
-
-    ratings_pred = Q.dot(P.T)
-    return ratings_pred.round(2)
-    """
