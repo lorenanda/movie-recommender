8561fe6ea66688b1bf7edd68ba16a7bf6f8374c4
DanielaMorariu1990 <57063213+DanielaMorariu1990@users.noreply.github.com>
2020-12-02 09:05:39 +0100

    change structure of application and moved to branch

--- falsk app/application.py
+++ falsk app/application.py
@@ -1,12 +1,14 @@
-from flask import Flask 
-from recommender import get_recommendations
+from flask import Flask
+from ml_models import nmf_recommand
 from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('main.html', title='Hello, World!')
+
 
 @app.route('/recommender')
 def recommender():
